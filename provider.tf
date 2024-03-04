bd02fb7365a6d8f0db315aaeb001ff4e4e4cbb76
Jagruthi111 <154959968+Jagruthi111@users.noreply.github.com>
2024-03-04 20:08:59 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+terraform {
+  backend "s3" {
+    region  = "ap-south-1"
+    bucket  = "backend-bucket567"
+    key     = "state.tfstate"
+	dynamodb_table = "backend-statelocking"
+    encrypt = true
+  }
+}
